0b4c5d73a98b2f46ebb2f1c98a3efff0761f2d2d
Aveheuzed <a.masson555@ntymail.com>
2019-04-10 11:46:19 +0200

    Etat du projet en cours de séance 3 de progra

--- projet.ino
+++ projet.ino
@@ -10,6 +10,12 @@
 #define NB_PORTES 5
 #define LONGUEUR_BARRE_LED 10
 
+void manche(const unsigned int n, const unsigned int k, bool *change, bool *gagne);
+void afficher(char chiffre);
+void choisir_porte(unsigned char etat[], unsigned int *i, char valeur);
+void gerer_victoire(bool *change, bool *gagne);
+
+
 const unsigned char PORT_LED_PORTE[NB_PORTES]={7,8,9,10,11};
 const unsigned char PORT_BOUTON_PORTE[NB_PORTES]={14,15,16,17,18};
 const unsigned char PORT_SERVO_PORTE[NB_PORTES]={19,20,21,22,23};
@@ -32,6 +38,8 @@ bool joueur = 0;
 void setup() {
   // put your setup code here, to run once:
 
+  Serial.begin(9600);
+
   pinMode(PORT_BOUTON, INPUT_PULLUP); // bouton par défaut HIGH, LOW quand appuyé
 
   barre_led_g.begin();
@@ -45,6 +53,7 @@ void setup() {
   {
     pinMode(PORT_BOUTON_PORTE[i], INPUT);
     led_porte[i]=Adafruit_NeoPixel(1, PORT_LED_PORTE[i], NEO_GRB + NEO_KHZ800);
+    pinMode(PORT_BOUTON_PORTE[i], INPUT_PULLUP);
     led_porte[i].begin();
     led_porte[i].show();
     servo_porte[i].attach(PORT_SERVO_PORTE[i]);
@@ -58,13 +67,19 @@ void setup() {
 
   randomSeed(analogRead(0));
 
+  Serial.println("setup fini"); Serial.flush();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  Serial.println("loop commencee"); Serial.flush();
   unsigned long last_change=millis();
-  while (last_change - millis() < 3000)
+  while ((last_change - millis()) < (unsigned long) 3000)
   {
+//    Serial.println((last_change - millis())); Serial.flush();
+    delay(10);
     if (digitalRead(PORT_SWITCH) != solo) // (solo != solo=digitalRead(PORT_SWITCH))
     {
       last_change = millis();
@@ -77,41 +92,51 @@ void loop() {
       game_mode%=4;
       afficher(game_mode+1 +4*(solo==0));
     }
-
-    bool change, gagne;
-    switch (game_mode)
-    {
-      case 0:
-        while(led[0]!=LONGUEUR_BARRE_LED && led[1]!=LONGUEUR_BARRE_LED) {
-          manche(3, 1, &change, &gagne);
-          gerer_victoire(&change, &gagne);
-        }
-        break;
-      case 1:
-        while(led[0]!=LONGUEUR_BARRE_LED && led[1]!=LONGUEUR_BARRE_LED) {
-          manche(5, 1, &change, &gagne);
-          gerer_victoire(&change, &gagne);
-        }
-        break;
-      case 2:
-        while(led[0]!=LONGUEUR_BARRE_LED && led[1]!=LONGUEUR_BARRE_LED) {
-          manche(5, 3, &change, &gagne);
-          gerer_victoire(&change, &gagne);
-        }
-        break;
-      case 3:
-        unsigned char increment[2]={0,0};
-        while(increment[0]<=2 && increment[1]<=2) {
-          manche(3+increment[joueur], 1+increment[joueur], &change, &gagne);
-          if(gagne) {increment[joueur]++;}
-          gerer_victoire(&change, &gagne);
-        }
-        break;
-    }
   }
+  Serial.println("while fini"); Serial.flush();
+
+  bool change, gagne;
+//  switch (game_mode)
+//  {
+//    case 0:
+//    Serial.println("case 0"); Serial.flush();
+//      while(led[0]!=LONGUEUR_BARRE_LED && led[1]!=LONGUEUR_BARRE_LED) {
+        manche(3, 1, &change, &gagne);
+        Serial.println("manche finie (case)"); Serial.flush();
+        gerer_victoire(&change, &gagne);
+        Serial.println("gerer_victoire finie"); Serial.flush();
+//      }
+//      break;
+//    case 1:
+//      Serial.println("case 0"); Serial.flush();
+//      while(led[0]!=LONGUEUR_BARRE_LED && led[1]!=LONGUEUR_BARRE_LED) {
+//        manche(5, 1, &change, &gagne);
+//        gerer_victoire(&change, &gagne);
+//      }
+//      break;
+//    case 2:
+//      Serial.println("case 0"); Serial.flush();
+//      while(led[0]!=LONGUEUR_BARRE_LED && led[1]!=LONGUEUR_BARRE_LED) {
+//        manche(5, 3, &change, &gagne);
+//        gerer_victoire(&change, &gagne);
+//      }
+//      break;
+//    case 3:
+//      Serial.println("case 0"); Serial.flush();
+//      unsigned char increment[2]={0,0};
+//      while(increment[0]<=2 && increment[1]<=2) {
+//        manche(3+increment[joueur], 1+increment[joueur], &change, &gagne);
+//        if(gagne) {increment[joueur]++;}
+//        gerer_victoire(&change, &gagne);
+//      }
+//      break;
+//  }
+  Serial.println("switch fini"); Serial.flush();
+  Serial.println("loop fini"); Serial.flush();
 }
 
 void manche(const unsigned int n, const unsigned int k, bool *change, bool *gagne) {
+  Serial.println("manche commencee"); Serial.flush();
   bool contenu[NB_PORTES] = {0,0,0,0,0}; // 5 étant le nombre max de portes
   unsigned char etat[NB_PORTES] = {0,0,0,0,0}; // 0 fermé ; 1 ouvert ; 2 choisi (a) ; 3 choisi (b)
   long voiture = random(n);
@@ -128,23 +153,35 @@ void manche(const unsigned int n, const unsigned int k, bool *change, bool *gagn
     }
   }
 
+  Serial.println("choix voiture OK"); Serial.flush();
+
   choisir_porte(etat, &i, 2);
+  
+  Serial.println("choix porte 1 OK"); Serial.flush();
 
   long pres;
   for(i=0 ; i++ ; i<k) {
     do {
       pres = random(n);
+      delay(10);
     } while(etat[pres] || contenu[pres]) ;
     etat[pres] = 1;
     servo_porte[pres].write(90); // par convention, 90° correspond à "porte ouverte"
     delay(500); //attendre 500 ms
   }
 
+  Serial.println("choix présentateur OK"); Serial.flush();
+
   choisir_porte(etat, &j, 3);
+
+  Serial.println("choix porte 2 OK"); Serial.flush();
+  
   servo_porte[j].write(90);
 
   *change = (bool) (i != j);
   *gagne = contenu[j];
+
+  Serial.println("manche finie"); Serial.flush();
 }
 
 // fonction écrivant sur un seul afficheur
@@ -173,6 +210,7 @@ void choisir_porte(unsigned char etat[], unsigned int *i, char valeur) {
         break;
       }
     }
+   delay(10);
   }
 }
 
